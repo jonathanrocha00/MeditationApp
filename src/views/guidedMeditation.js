52c3f72fc0d947b57d452af44ffc37bcba982549
Jonathan Rocha <jonathan.rocha@msn.com>
2020-11-16 18:47:10 -0300

    Add playing music functionality

--- src/views/guidedMeditation.js
+++ src/views/guidedMeditation.js
@@ -1,34 +1,68 @@
 import * as React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableHighlight } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-
+import { Audio } from 'expo-av';
 
 const guidedMeditation = ({ navigation }) => {
+
+	const meditationAudio = new Audio.Sound();
+
+	React.useEffect(() => {
+		try {
+			meditationAudio.loadAsync(require('../assets/music/1997.mp3'));
+		} catch (error) {
+			// Forgive me Roberta 
+		}
+
+		return () => {
+			meditationAudio.unloadAsync();
+		};
+	});
+
 	return (
 		<View style={styles.container}>
 			<LinearGradient
-                colors={['#66ccff', 'transparent']}
-                style={{
-                    position: 'absolute',
-                    left: 0,
-                    right: 0,
-                    top: 0,
-                    height: 600,
-                }}
-            />
+				colors={['#66ccff', 'transparent']}
+				style={{
+					position: 'absolute',
+					left: 0,
+					right: 0,
+					top: 0,
+					height: 600,
+				}}
+			/>
 			<View style={styles.titleContainer}>
 				<Image source={require('../assets/images/meditation.png')} style={{ width: 80, height: 80 }} />
 				<Text style={styles.title}> Título </Text>
 			</View>
 			<View style={styles.timerContainer}>
-				<Text style={styles.timer}> 88:88 </Text>
+				<Text style={styles.timer}> 89:88 </Text>
 			</View>
 			<View style={styles.controlsContainer}>
-				<Image source={require('../assets/images/back15seconds.png')} style={{ width: 40, height: 40 }} />
-				<Image source={require('../assets/images/play.png')} style={{ width: 80, height: 80 }} />
-				<Image source={require('../assets/images/stop.png')} style={{ width: 40, height: 40 }} />
+				<TouchableHighlight
+					onPress={async () => {
+						let audioState = await meditationAudio.getStatusAsync();
+						console.log(audioState);
+						await meditationAudio.setPositionAsync(audioState.positionMillis - 15000);
+					}}
+				>
+					<Image source={require('../assets/images/back15seconds.png')} style={{ width: 40, height: 40 }} />
+				</TouchableHighlight>
+
+				<TouchableHighlight
+					onPress={async () => await meditationAudio.playAsync()}
+				>
+					<Image source={require('../assets/images/play.png')} style={{ width: 80, height: 80 }} />
+				</TouchableHighlight>
+
+				<TouchableHighlight
+					onPress={async () => await meditationAudio.stopAsync()}
+				>
+					<Image source={require('../assets/images/stop.png')} style={{ width: 40, height: 40 }} />
+				</TouchableHighlight>
+				
 			</View>
-		</View>
+		</View >
 	);
 }
 
