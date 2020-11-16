997885b67d9325474019e30db1473048227401f7
Jonathan Rocha <jonathan.rocha@msn.com>
2020-11-15 21:49:37 -0300

    Add navigation and home screen

--- App.js
+++ App.js
@@ -1,17 +1,86 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-// import Selection from '.src/views/selection/index';
+import * as React from 'react';
+import { Button, View, Text, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { LinearGradient } from 'expo-linear-gradient';
+import DetailsScreen from './src/views/Details';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
+function HomeScreen({ navigation }) {
     return (
-      <View>
-        <Text> lala </Text>
-      </View>
+        <View style={styles.home}>
+            <LinearGradient
+                // Background Linear Gradient
+                colors={['#d9b3ff', 'transparent']}
+                style={{
+                    position: 'absolute',
+                    left: 0,
+                    right: 0,
+                    top: 0,
+                    height: 600,
+                }}
+            />
+            <Text style={styles.homeTitle}>
+                Bem vinda de volta
+            </Text>
+
+            <Text style={styles.homeQuote}>
+                "Você é uma estrela de luz. Você merece ser feliz!"
+            </Text>
+
+            <Button
+                title="Relaxar"
+                onPress={() => navigation.navigate('Details')}
+            />
+        </View>
     );
-  }
 }
+
+const Stack = createStackNavigator();
+
+function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+                <Stack.Screen name="Details" component={DetailsScreen} options={{ headerShown: false }}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+    home: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#66ccff',
+        padding: 40
+    },
+    homeTitle: {
+        color: 'white',
+        fontSize: 30,
+        fontWeight: 'bold',
+        marginBottom: 50
+    },
+    homeQuote: {
+        color: 'white',
+        fontSize: 16,
+        fontStyle: 'italic',
+        marginBottom: 100
+    },
+    textInput: {
+        padding: 10,
+        width: 300,
+        margin: 10,
+        borderWidth: 1,
+        borderColor: 'grey',
+    },
+    person: {
+        marginTop: 20,
+        padding: 20,
+        fontSize: 20,
+        backgroundColor: 'grey',
+    },
+});
